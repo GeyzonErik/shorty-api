437544ef25a2a8529cb98f60b5c323f268badafc
GeyzonErik <Geyzon100@gmail.com>
2025-05-16 14:13:01 -0300

    feat: add Swagger configuration and new dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Shorty-Api')
+    .setDescription(
+      'Documentação simples para API do Shorty, um encurtador de Urls',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
