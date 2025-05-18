2e1b55b34ab3450bf3d916742ffec0238ce5a228
GeyzonErik <Geyzon100@gmail.com>
2025-05-18 03:44:57 -0300

    fix: update import path for ToggleUrlStatus use case

--- src/modules/urls/url.module.ts
+++ src/modules/urls/url.module.ts
@@ -9,7 +9,7 @@ import { RedirectUrlController } from './api/controllers/redirect-url.controller
 import { GetOrignalUrl } from './application/usecases/get-original-url.usecase';
 import { ListUserUrls } from './application/usecases/list-user-urls.usecase';
 import { UpdateUrl } from './application/usecases/update-url.usecase';
-import { ToggleUrlStatus } from './application/usecases/deactivate-url.usecase';
+import { ToggleUrlStatus } from './application/usecases/toggle-url-status.usecase';
 import { DeleteUrl } from './application/usecases/delete-url.usecase';
 
 @Module({
