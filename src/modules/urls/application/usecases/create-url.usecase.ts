fc8e31e12534f158e1f04d1435dd25e43a1bc5bb
GeyzonErik <Geyzon100@gmail.com>
2025-05-18 00:29:58 -0300

    feat: implement CreateUrl use case for URL creation and management

---
+++ src/modules/urls/application/usecases/create-url.usecase.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { IUrlRepository } from '../repositories/urls.repository';
+import { UrlBuilder } from '@/urls/domain/builders/url.builder';
+import { User } from '@/users/domain/entities/user.entity';
+
+export type CreateUrlInput = {
+  user?: User;
+  originalUrl: string;
+};
+
+export type CreateUrlOutput = {
+  slug: string;
+  visits: number;
+};
+
+@Injectable()
+export class CreateUrl {
+  @Inject(IUrlRepository)
+  private readonly urlRepo: IUrlRepository;
+
+  async execute(data: CreateUrlInput): Promise<CreateUrlOutput> {
+    const url = await UrlBuilder.create(data).withUniqueSlug(
+      this.urlRepo.existsSlug.bind(this.urlRepo),
+    );
+
+    console.log(url);
+
+    const newUrl = await this.urlRepo.create(url);
+
+    return {
+      slug: newUrl.slug,
+      visits: newUrl.visits,
+    };
+  }
+}
